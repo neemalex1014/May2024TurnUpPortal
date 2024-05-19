848fdad4e832220c6af387256ce88774372c4021
Neema Alex <neemalex1014@gmail.com>
2024-05-19 15:48:32 +1200

    TurnUp Portal Login Script

---
+++ May2024TurnUpPortal/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+IWebElement userNameElement = driver.FindElement(By.Id("UserName"));
+userNameElement.SendKeys("hari");
+
+IWebElement passwordElement = driver.FindElement(By.Id("Password"));
+passwordElement.SendKeys("123123");
+
+IWebElement loginButtonElement = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButtonElement.Click();
